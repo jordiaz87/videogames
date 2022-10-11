b377992e356712ab6520d139e94b31a3c4044b36
Jorge Diaz <jorjorf@gmail.com>
2022-10-11 17:13:32 -0400

    inicio de creacion del canvas

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+    // game.fillRect(0,50,100,100);
+    // game.clearRect(50,50,50,50);
+
+    game.font = '25px Verdana';
+    game.fillStyle = 'Purple';
+    game.textAlign = 'center';
+    game.fillText('Platzi', 25, 25);
+    
+}
