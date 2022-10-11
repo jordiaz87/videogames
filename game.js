af0b101f67f809da74d33429c6c06b525d26744a
Jorge Diaz <jorjorf@gmail.com>
2022-10-11 18:01:50 -0400

    Tamaño del canvas y sus elementos

--- game.js
+++ game.js
@@ -4,12 +4,41 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame);
 
 function startGame() {
+
+    let canvasSize;
+
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const elementsSize = canvasSize / 10;
+
+    console.log({ canvasSize, elementsSize});
+
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = 'end';
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementsSize * i , elementsSize );
+        
+        
+    }
+
+    
+
     // game.fillRect(0,50,100,100);
     // game.clearRect(50,50,50,50);
 
-    game.font = '25px Verdana';
-    game.fillStyle = 'Purple';
-    game.textAlign = 'center';
-    game.fillText('Platzi', 25, 25);
+    // window.innerWidth
+
+    // game.font = '25px Verdana';
+    // game.fillStyle = 'Purple';
+    // game.textAlign = 'center';
+    // game.fillText('Platzi', 25, 25);
     
 }
