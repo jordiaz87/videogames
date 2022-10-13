1cab57c52faad61a95e1daa39ca6e6a9f8b845dc
Jorge Diaz <jorjorf@gmail.com>
2022-10-12 22:03:38 -0400

    Arreglos multidimensionales en JavaScript01

--- game.js
+++ game.js
@@ -24,8 +24,16 @@ function startGame() {
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
-    for (let i = 1; i <= 10; i++) {
-        game.fillText(emojis['X'], elementsSize * i , elementsSize );
+    const map = maps[0];
+    const mapRows = map.trim().split('\n');
+    const mapRowCols = mapRows.map(row => row.trim().split(''));
+    console.log({map, mapRows, mapRowCols});
+
+    for (let row = 1; row <= 10; row++) {
+        for (let col = 0; col <= 10; col++) {
+            game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+        }
+        
     }
 }
 
