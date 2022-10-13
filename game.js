0acf77de3f0d5eef5d38ba5d7049b23c25cb39d1
Jorge Diaz <jorjorf@gmail.com>
2022-10-12 20:54:49 -0400

    Canvas responsive

--- game.js
+++ game.js
@@ -1,44 +1,31 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
 
-function startGame() {
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-    let canvasSize;
-
+function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.8;
     } else {
         canvasSize = window.innerHeight * 0.8;
     }
-
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
+    elementsSize = canvasSize / 10;
+    startGame();
+}
 
-    const elementsSize = canvasSize / 10;
-
+function startGame() {
     console.log({ canvasSize, elementsSize});
-
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
     for (let i = 1; i <= 10; i++) {
         game.fillText(emojis['X'], elementsSize * i , elementsSize );
-        
-        
     }
-
-    
-
-    // game.fillRect(0,50,100,100);
-    // game.clearRect(50,50,50,50);
-
-    // window.innerWidth
-
-    // game.font = '25px Verdana';
-    // game.fillStyle = 'Purple';
-    // game.textAlign = 'center';
-    // game.fillText('Platzi', 25, 25);
-    
 }
+
