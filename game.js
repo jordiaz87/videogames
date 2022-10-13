e90d45469f07e8ffa1d219f93ee8320180c1a362
Jorge Diaz <jorjorf@gmail.com>
2022-10-13 18:08:33 -0400

    Objeto playerPosition

--- game.js
+++ game.js
@@ -11,6 +11,11 @@ const btnDown = document.querySelector('#down');
 let canvasSize;
 let elementsSize;
 
+const playerPosition = {
+    x: undefined,
+    y: undefined,
+};
+
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
@@ -41,12 +46,23 @@ function startGame() {
             const emoji = emojis[col];
             const posX = elementsSize * (colI + 1);
             const posY = elementsSize * (rowI + 1);
+
+            if (col == 'O') {
+                playerPosition.x = posX;
+                playerPosition.y = posY;
+                console.log(playerPosition);
+            }
             game.fillText(emoji, posX, posY);
-            console.log({ row, rowI, col, colI});
+            
         });
     });
 
-    
+    movePlayer();
+
+}
+
+function movePlayer() {
+    game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
 }
 
 btnUp.addEventListener('click', moveUp);
@@ -56,6 +72,8 @@ btnDown.addEventListener('click', moveDown);
 
 function moveUp() {
     console.log('arriba');
+    playerPosition.y -= elementsSize;
+    movePlayer();
 }
 
 function moveLeft() {
