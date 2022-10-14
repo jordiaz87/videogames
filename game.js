49740f2361d1c1027959d32e85655a523b6be46a
Jorge Diaz <jorjorf@gmail.com>
2022-10-14 09:09:56 -0400

    Detectando colisiones fijas

--- game.js
+++ game.js
@@ -16,6 +16,11 @@ const playerPosition = {
     y: undefined,
 };
 
+const giftPosition = {
+    x: undefined,
+    y: undefined,
+}
+
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
@@ -36,7 +41,7 @@ function startGame() {
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
-    const map = maps[0];
+    const map = maps[1];
     const mapRows = map.trim().split('\n');
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({map, mapRows, mapRowCols});
@@ -55,6 +60,10 @@ function startGame() {
                 playerPosition.y = posY;
                 console.log(playerPosition);
                }
+            } else if (col == 'I') {
+                giftPosition.x = posX;
+                giftPosition.y = posY;
+
             }
             game.fillText(emoji, posX, posY);
             
@@ -66,6 +75,15 @@ function startGame() {
 }
 
 function movePlayer() {
+    const giftCollisionX = playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3);
+    const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3);
+    const giftCollision = giftCollisionX && giftCollisionY;
+
+    if (giftCollision) {
+        console.log('subiste de nivel');
+    }
+
+   
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
 }
 
