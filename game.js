e8c222d77eb134037befedeb5b069aa9fcea5a4d
Jorge Diaz <jorjorf@gmail.com>
2022-10-14 23:12:20 -0400

    Derrota: perdiendo vidas

--- game.js
+++ game.js
@@ -11,6 +11,7 @@ const btnDown = document.querySelector('#down');
 let canvasSize;
 let elementsSize;
 let level = 0;
+let lives = 3;
 
 const playerPosition = {
     x: undefined,
@@ -104,7 +105,7 @@ function movePlayer() {
     });
 
     if (enemyCollision) {
-        console.log('chocaste contra un enemigo');
+        levelFail();
     }
    
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
@@ -114,6 +115,23 @@ function levelWin() {
     console.log('Subiste de Nivel');
     level++;
     startGame();
+}
+
+function levelFail() {
+    console.log('chocaste en contra de un enemigo');
+
+    lives--;
+
+    console.log(lives);
+    if (lives <= 0) {
+        level = 0;
+        lives = 3;
+    }
+
+    playerPosition.x = undefined;
+    playerPosition.y = undefined;
+    startGame();
+    
 }
 
 function gameWin() {
